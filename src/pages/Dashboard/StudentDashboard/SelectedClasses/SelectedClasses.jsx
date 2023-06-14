40fe67c06177dfb4662f075d20527d54e3c3ba6c
abdullahtusar <abdullahtusar2@gmail.com>
2023-06-14 19:29:45 +0600

    In SelectedClasses remove button function added

--- src/pages/Dashboard/StudentDashboard/SelectedClasses/SelectedClasses.jsx
+++ src/pages/Dashboard/StudentDashboard/SelectedClasses/SelectedClasses.jsx
@@ -1,8 +1,37 @@
 import { Link } from "react-router-dom";
 import useSelectedClasses from "../../../../hooks/useSelectedClasses";
+import Swal from "sweetalert2";
+
 
 const SelectedClasses = () => {
-    const [selectedClasses] = useSelectedClasses()
+    const [selectedClasses, , refetch] = useSelectedClasses();
+    const handleDelete = item =>{
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              fetch(`http://localhost:5000/selected_classes/${item._id}`, {
+                method: 'DELETE'
+              })
+              .then(res => res.json())
+              .then(data => {
+                if(data.deletedCount > 0) {
+                    refetch();
+                    Swal.fire(
+                        'Deleted!',
+                        'Your file has been deleted.',
+                        'success')
+                }
+              })
+            }
+          })
+    }
     return (
         <div className='w-11/12 mx-auto'>
             <h2 className="text-3xl font-bold text-center text-[#24a9e1]">MY SELECTED CLASSES</h2>
@@ -44,7 +73,7 @@ const SelectedClasses = () => {
                                     <Link to={`/dashboard/payment?id=${own_class._id}&price=${own_class.price}`}>
                                         <button className="btn btn-sm btn-neutral bg-green-600 border-0  mr-5 text-white">Pay</button>
                                     </Link>
-                                    <button className="btn btn-sm btn-neutral bg-red-600 border-0 text-white">Remove</button>
+                                    <button onClick={() => handleDelete(own_class)} className="btn btn-sm btn-neutral bg-red-600 border-0 text-white">Remove</button>
                                 </td>
                             </tr>)
                         }
